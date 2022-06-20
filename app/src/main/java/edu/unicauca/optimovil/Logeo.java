2226b18e1b20e1c7f9276cd271dc1bf9b8ae688b
Hernan Trullo <trullodario@gmail.com>
2022-06-20 15:00:15 -0500

    Logeo y control de pass y user

--- app/src/main/java/edu/unicauca/optimovil/Logeo.java
+++ app/src/main/java/edu/unicauca/optimovil/Logeo.java
@@ -4,7 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import edu.unicauca.optimovil.Actividades.VentanaClientes;
+import edu.unicauca.optimovil.Actividades.VentanaPrincipal;
+import edu.unicauca.optimovil.Actividades.VentanaProducto;
 
 public class Logeo extends AppCompatActivity {
 
@@ -16,8 +24,33 @@ public class Logeo extends AppCompatActivity {
         // Se los elementos de la ventana con el script
         EditText txtUser = findViewById(R.id.txt_email);
         EditText txtPass = findViewById(R.id.txt_contrasena);
+        Button btnLogeo = findViewById(R.id.btn_login);
 
-        Intent intent = getIntent();
+        btnLogeo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (esValidaUserPass(txtUser.getText().toString(), txtPass.getText().toString())){
+                    Intent intent = new Intent(Logeo.this, VentanaClientes.class);
+                    startActivity(intent);
+                }
+                else{
+                    Toast.makeText( Logeo.this, R.string.txt_mensaje_error_logeo, Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+    }
+
+    private boolean esValidaUserPass(String user, String pass) {
+        if (user.contains("trullodario@gmail.com") && pass.contains("1007587458")){
+
+            return true;
+        }
+        else {
+            return false;
+        }
+
     }
 
 
