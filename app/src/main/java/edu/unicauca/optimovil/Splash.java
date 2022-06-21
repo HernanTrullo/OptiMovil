ba9bc250fad4fa532e429c0113296044d37b9112
Diana Carolina Mariaca Alvares <dianacaroma@unicauca.edu.co>
2022-06-20 21:26:30 -0500

    Arreglos en la presentación de las ventanas

--- app/src/main/java/edu/unicauca/optimovil/Splash.java
+++ app/src/main/java/edu/unicauca/optimovil/Splash.java
@@ -1,11 +1,10 @@
 package edu.unicauca.optimovil;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -15,12 +14,19 @@ import edu.unicauca.optimovil.Actividades.VentanaPrincipal;
 public class Splash extends Activity {
 
     private ProgressBar spinner;
+    private TextView NombreEmpresa;
+    private TextView lema;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
         spinner = (ProgressBar)findViewById(R.id.progressBar);
+        NombreEmpresa = findViewById(R.id.Nombre_aplicacion);
+        lema = findViewById(R.id.Lema_aplicacion);
+        NombreEmpresa.setText("Optivisión del norte");
+        lema.setText(R.string.lema_app);
+        lema.setEnabled(true);
 
         TimerTask timerTask = new TimerTask() {
             @Override
